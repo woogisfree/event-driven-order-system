b7e413eea4a8c7d7d03fcf42fdf0a7b5eac22059
woogie <woogisfree@gmail.com>
2024-11-08 18:07:40 +0900

    feat: initial commit

---
+++ src/main/java/io/woogisfree/eventdrivenordersystem/EventDrivenOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package io.woogisfree.eventdrivenordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/woogisfree/eventdrivenordersystem/EventDrivenOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.woogisfree.eventdrivenordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
