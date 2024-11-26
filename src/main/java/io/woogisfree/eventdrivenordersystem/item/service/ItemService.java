239773962429dad5eda0ee64e46e3b23dd7ecc9c
woogie <woogisfree@gmail.com>
2024-11-27 00:11:40 +0900

    refactor: Simplify service layer by removing interfaces

--- src/main/java/io/woogisfree/eventdrivenordersystem/item/service/ItemService.java
+++ src/main/java/io/woogisfree/eventdrivenordersystem/item/service/ItemService.java
@@ -1,19 +1,58 @@
 package io.woogisfree.eventdrivenordersystem.item.service;
 
+import io.woogisfree.eventdrivenordersystem.item.domain.Item;
 import io.woogisfree.eventdrivenordersystem.item.dto.ItemResponse;
+import io.woogisfree.eventdrivenordersystem.item.exception.ItemNotFoundException;
+import io.woogisfree.eventdrivenordersystem.item.mapper.ItemMapper;
+import io.woogisfree.eventdrivenordersystem.item.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public interface ItemService {
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class ItemService {
 
-    Long saveItem(String name, int price, int stockQuantity);
+    private final ItemRepository itemRepository;
+    private final ItemMapper itemMapper;
 
-    ItemResponse findItem(Long itemId);
+    @Transactional
+    public Long saveItem(String name, int price, int stockQuantity) {
+        Item item = Item.builder()
+                .name(name)
+                .price(price)
+                .stockQuantity(stockQuantity)
+                .build();
+        return itemRepository.save(item).getId();
+    }
 
-    List<ItemResponse> findItems();
+    public ItemResponse findItem(Long itemId) {
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new ItemNotFoundException("Item with ID " + itemId + " does not exist."));
+        return itemMapper.toDto(item);
+    }
 
-    void updateItem(Long itemId, String name, int price, int stockQuantity);
+    public List<ItemResponse> findItems() {
+        List<Item> items = itemRepository.findAll();
+        return itemMapper.toDtoList(items);
+    }
 
-    void deleteItem(Long itemId);
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new ItemNotFoundException("Item with ID " + itemId + " does not exist."));
+        item.update(name, price, stockQuantity);
+    }
+
+    // TODO: 주문 목록에 해당 상품이 존재하는지 확인하는 로직 추가
+    @Transactional
+    public void deleteItem(Long itemId) {
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new ItemNotFoundException("Item with ID " + itemId + " does not exist."));
+        itemRepository.delete(item);
+    }
 
 }
--- src/main/java/io/woogisfree/eventdrivenordersystem/member/service/MemberService.java
+++ src/main/java/io/woogisfree/eventdrivenordersystem/member/service/MemberService.java
@@ -1,13 +1,52 @@
 package io.woogisfree.eventdrivenordersystem.member.service;
 
+import io.woogisfree.eventdrivenordersystem.member.domain.Member;
 import io.woogisfree.eventdrivenordersystem.member.dto.MemberResponse;
+import io.woogisfree.eventdrivenordersystem.member.exception.MemberNotFoundException;
+import io.woogisfree.eventdrivenordersystem.member.mapper.MemberMapper;
+import io.woogisfree.eventdrivenordersystem.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface MemberService {
-    Long createMember(String name, String address);
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class MemberService {
 
-    MemberResponse findMemberWithOrders(Long memberId);
+    private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
 
-    void updateMember(Long memberId, String name, String address);
+    @Transactional
+    public Long createMember(String name, String address) {
+        Member member = Member.builder()
+                .name(name)
+                .address(address)
+                .build();
+        memberRepository.save(member);
+        return member.getId();
+    }
 
-    void deleteMember(Long memberId);
+    public MemberResponse findMemberWithOrders(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException("Member with ID " + memberId + " does not exist."));
+        return memberMapper.toDto(member);
+    }
+
+    @Transactional
+    public void updateMember(Long memberId, String name, String address) {
+        memberRepository.findById(memberId)
+                .ifPresentOrElse(member -> {
+                    member.update(name, address);
+                }, () -> {
+                    throw new MemberNotFoundException("Member with ID " + memberId + " does not exist.");
+                });
+    }
+
+    @Transactional
+    public void deleteMember(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException("Member with ID " + memberId + " does not exist."));
+        memberRepository.delete(member);
+    }
 }
